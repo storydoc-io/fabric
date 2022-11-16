3bbcc072bd11d7101d246f10dae7db0248c25fbb
Bram Stieperaere <bram@storydoc.io>
2022-11-16 14:30:39 +0100

    feedback 0.1.0 key uniqueness validation in settings should be case-insensitive

--- fabric-frontend/src/app/common/validators.ts
+++ fabric-frontend/src/app/common/validators.ts
@@ -3,7 +3,7 @@ import {AbstractControl, ValidationErrors, ValidatorFn} from "@angular/forms";
 export const unique = function (fieldName: string, values: string[]): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
         let controlValue: string = control.value;
-        const invalid = controlValue && !!values.find(val => val === controlValue)
+        const invalid = controlValue && !!values.find(val => val && controlValue && val.toUpperCase() === controlValue.toUpperCase())
         return invalid ? {unique: { value: `The ${fieldName} '${controlValue}' is already in use.`}} : null;
     }
 }
